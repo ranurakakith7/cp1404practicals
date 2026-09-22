818ab0097973ed7b8d7ae56cd76c65b59042c0f5
kithn <ranurakakithnula@gmail.com>
2026-09-22 11:50:55 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,19 @@
-password = input("Password: ")
-min_length = 8
-while len(password) < min_length:
-    print("Invalid, needs to be at least 8 characters")
+def main():
+    min_length = 8
+    password = get_password(min_length)
+    print_asterisks(password)
+
+
+def print_asterisks(password: str):
+    print("*" * len(password))
+
+
+def get_password(min_length: int) -> str:
     password = input("Password: ")
-print("*" * len(password))
+    while len(password) < min_length:
+        print("Invalid, needs to be at least 8 characters")
+        password = input("Password: ")
+    return password
+
+
+main()
